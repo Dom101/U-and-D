0074d5aee21bcd34f73f4e4f1e2a4b9ebbd13b30
unknown <James@JamesNetBook.(none)>
2011-12-09 14:44:36 +0000

    added initial cell class

--- Code/Uppers and Downers/src/com/groupD/corepackage/SpecialCell.java
+++ Code/Uppers and Downers/src/com/groupD/corepackage/SpecialCell.java
@@ -6,8 +6,22 @@ package com.groupD.corepackage;
 
 /**
  *
- * @author fayimora
+ * @author Dominic
  */
-public class SpecialCell {
+public class SpecialCell extends Cell{
     
+    public enum cellType {
+        UPPER, DOWNER, BONUS
+    }
+    
+    cellType type;
+    
+    public SpecialCell(int cellnumber, cellType type){
+        super(cellnumber);
+        this.type = type;
+    }
+    
+    public Enum getCellType(){
+        return type;
+    }
 }
